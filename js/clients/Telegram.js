21136b5e4593707fc690aaa44299c4206b13d591
tabby-jun <6513021+tabby-jun@users.noreply.github.com>
2019-08-22 18:26:25 +0000

    implement a way to set commands and messages to reply with, starting with /help

--- js/clients/Telegram.js
+++ js/clients/Telegram.js
@@ -1,15 +1,19 @@
 const Telegraf = require('telegraf')
 const pjson = require('../../package.json')
+const commandMsg = require('../../messages/commandMsg')
 
 module.exports = {
   'createClient': createClient
 }
+
+var messageMarkdownOption = { 'parse_mode': 'HTML' }
 
 function createClient () {
   const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
   bot.command('version', replyVersion)
   bot.start(replyStart)
+  setCommandReplies(bot)
   bot.launch()
   return bot
 }
@@ -19,5 +23,18 @@ function replyVersion (context) {
 }
 
 function replyStart (context) {
-  context.reply(`I am currently running. My teletenor version is ${pjson.version}`)
+  context.reply(`I am currently running now! My teletenor version is ${pjson.version}`)
+}
+
+function setCommandReplies (bot) {
+  var commandKeys = Object.keys(commandMsg)
+  for (var key of commandKeys) {
+    bot.command(key, createReplyMessageFunc(commandMsg[key]))
+  }
+}
+
+function createReplyMessageFunc (message) {
+  return function (context) {
+    context.reply(message, messageMarkdownOption)
+  }
 }
---
+++ messages/commandMsg.js
@@ -0,0 +1,16 @@
+
+const commandMsg = {
+  'help': `<b>What can teletenor do?</b>
+
+  Mention @ttenor_bot for inline tenor.com GIF search for telegram!
+
+  <b>Example</b>
+  Type <code>@ttenor_bot cute cats</code>
+  Cat GIF suggestions will appear!
+  Select a GIF to post them in your chat!
+
+  This bot is open-source too! <a href="https://git.io/fj5xN">Github: teletenor</a>
+  `
+}
+
+module.exports = commandMsg
