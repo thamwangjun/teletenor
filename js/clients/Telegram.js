367b1edc2a6a522b6ca542e5c40874cdcf4730fc
tabby-jun <6513021+tabby-jun@users.noreply.github.com>
2019-08-24 12:25:51 +0000

    refactor logging and share related code

--- js/clients/Telegram.js
+++ js/clients/Telegram.js
@@ -1,10 +1,8 @@
 const Telegraf = require('telegraf')
-const Bottleneck = require('bottleneck')
 const Logger = require('../logger/Logger')
 const pjson = require('../../package.json')
 const commandMsg = require('../messages/commandMsg')
 
-const registerSharePerSecond = process.env.REGISTER_SHARE_PER_SECOND || 100
 const loggingEnabled = process.env.LOGGING_ENABLED || false
 
 module.exports = {
@@ -20,25 +18,10 @@ function createClient (tenorClient) {
   bot.command('version', replyVersion)
   bot.start(replyStart)
   setCommandReplies(bot)
-
-  listenForChosenInlineResult(bot, registerSharePerSecond, tenorClient)
   useLogger(bot)
 
   bot.launch()
   return bot
-}
-
-function listenForChosenInlineResult (bot, tenorClient) {
-  var limiter = new Bottleneck(createBottleneckOptions())
-
-  var wrappedInlineResultHandler = limiter.wrap(handleChosenInlineResult.bind(bot))
-
-  bot.use((context, next) => {
-    if (context.chosenInlineResult) {
-      return wrappedInlineResultHandler(context, next)
-    }
-    return next(context)
-  })
 }
 
 function useLogger (bot) {
@@ -47,15 +30,6 @@ function useLogger (bot) {
     bot.use(Logger.logInline)
     bot.use(Logger.logRegisterShare)
   }
-}
-
-function handleChosenInlineResult (context, next) {
-  var chosenInlineResult = context.chosenInlineResult
-
-  this.tenorClient.registerShare(chosenInlineResult.query, chosenInlineResult.result_id, chosenInlineResult.from.language_code)
-    .then(function () {
-      return next(context)
-    })
 }
 
 function replyVersion (context) {
@@ -76,14 +50,5 @@ function setCommandReplies (bot) {
 function createReplyMessageFunc (message) {
   return function (context) {
     context.reply(message, messageMarkdownOption)
-  }
-}
-
-function createBottleneckOptions () {
-  var minimumTimePerRequest = 1000 / registerSharePerSecond
-  return {
-    minTime: minimumTimePerRequest,
-    highWater: registerSharePerSecond,
-    strategy: Bottleneck.strategy.LEAK
   }
 }
--- js/main/main.js
+++ js/main/main.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
-var Telegram = require('../clients/Telegram')
-var Tenor = require('../clients/Tenor')
+const Telegram = require('../clients/Telegram')
+const Tenor = require('../clients/Tenor')
 const queryProcessorFactory = require('../factories/QueryProcessorFactory')
+const TenorShareRegistrar = require('../functions/TenorShareRegistrar')
+
+const registerSharePerSecond = process.env.REGISTER_SHARE_PER_SECOND || 100
 
 function start () {
   var tenorClient = Tenor.createClient(process.env.TENOR_CONTENT_FILTER, 'basic')
@@ -10,6 +13,7 @@ function start () {
   var queryProcessor = queryProcessorFactory.createQueryProcessor(tenorClient)
 
   telegramClient.on('inline_query', queryProcessor.processQuery)
+  TenorShareRegistrar.listenForChosenInlineResult(telegramClient, registerSharePerSecond, tenorClient)
 }
 
 start()
