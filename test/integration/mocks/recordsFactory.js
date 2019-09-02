1b6f3785c557c2e156099d42ba2820a3792614a1
tabby-jun <6513021+tabby-jun@users.noreply.github.com>
2019-09-02 13:48:33 +0000

    partial implement of integration tests #01

---
+++ test/integration/mocks/recordsFactory.js
@@ -0,0 +1,25 @@
+var update = {
+  update_id: null,
+  inline_query: null
+}
+
+var inlineQuery = {
+  id: 'inline-query-001',
+  from: null,
+  query: '',
+  offset: 12
+}
+
+var records = {
+  Records: [
+    { messageId: '0001',
+      receiptHandle: 'receiptHandle-001',
+      body: '{}',
+      attributes: {},
+      messageAttributes: {},
+      md5OfBody: '8654031766',
+      eventSource: 'aws:sqs',
+      eventSourceARN: 'arn:aws:sqs:moon-01:001:queue',
+      awsRegion: 'moon-01' }
+  ]
+}
